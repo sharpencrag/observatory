2271cf2ef3a5e77f7007f3428bf435fca9425192
sharpencrag <ed.whetstone@gmail.com>
2023-04-16 16:36:51 -0700

    better annotations

--- observatory/data_types.py
+++ observatory/data_types.py
@@ -8,12 +8,27 @@ from typing import (
     overload,
     Hashable,
     Callable,
+    Iterable,
+    Protocol,
+    Mapping
 )
 
 from .events import EventHook
 
 
 T = TypeVar("T")
+
+K = TypeVar("K", bound=Hashable)
+
+
+class Sortable(Protocol):
+    """Protocol for defining sortable objects."""
+
+    def __gt__(self, other: Any) -> bool:
+        ...
+
+    def __lt__(self, other: Any) -> bool:
+        ...
 
 
 class NotSet:
@@ -67,66 +82,75 @@ class ObservableAttr(Generic[T]):
         instance.__dict__[self] = value
 
 
-class ObservableList(list):
+class ObservableList(list[T]):
     """Interface to a list that makes operations observable."""
 
-    list_item_set: EventHook[int, Any] = EventHook()
-    list_item_appended: EventHook[Any] = EventHook()
-    list_extended: EventHook[list[Any]] = EventHook()
-    list_item_inserted: EventHook[int, Any] = EventHook()
-    list_item_popped: EventHook[int, Any] = EventHook()
-    list_item_removed: EventHook[Any] = EventHook()
+    list_item_set: EventHook[int, T] = EventHook()
+    list_item_appended: EventHook[T] = EventHook()
+    list_extended: EventHook[list[T]] = EventHook()
+    list_item_inserted: EventHook[int, T] = EventHook()
+    list_item_popped: EventHook[int, T] = EventHook()
+    list_item_removed: EventHook[T] = EventHook()
     list_reversed = EventHook()
     list_sorted = EventHook()
     list_cleared = EventHook()
     list_changed = EventHook()
 
+    # required in order to act as a binding object for event hooks
     def __hash__(self):
-        # required in order to act as a binding object for event hooks
         return hash(id(self))
 
-    def __setitem__(self, index, value):
+    def __setitem__(self, index: int, value: T):
         super().__setitem__(index, value)
         self.list_item_set.emit(index, value)
         self.list_changed.emit()
 
-    def append(self, item):
+    def append(self, item: T):
+        """Append the given item to the end of the list."""
         super().append(item)
         self.list_changed.emit()
         self.list_item_appended.emit(item)
 
-    def extend(self, items):
+    def extend(self, items: Iterable[T]):
+        """Extend the list in-place."""
+        items = list(items)
         super().extend(items)
         self.list_changed.emit()
         self.list_extended.emit(items)
 
-    def insert(self, index, item):
+    def insert(self, index: int, item: T):
+        """Insert the item at the given index."""
         super().insert(index, item)
         self.list_changed.emit()
         self.list_item_inserted.emit(index, item)
 
-    def pop(self, index=-1):
+    def pop(self, index: int=-1) -> T:
+        """Remove and return the item at the given index."""
         item = super().pop(index)
         self.list_item_popped.emit(index, item)
         self.list_changed.emit()
         return item
 
-    def remove(self, item):
+    def remove(self, item: T):
+        """Remove the given item from the list."""
         super().remove(item)
         self.list_item_removed.emit(item)
         self.list_changed.emit()
 
     def reverse(self):
+        """Reverse the list in-place."""
         super().reverse()
         self.list_reversed.emit()
         self.list_changed.emit()
 
-    def sort(self, key=None, reverse=False):
-        super().sort(key=key, reverse=reverse)
+    def sort(self, key: Optional[Callable[[T], Sortable]]=None, reverse=False):
+        """Sort the list in-place."""
+        super().sort(key=key, reverse=reverse)  # type: ignore (typeshed bug)
         self.list_sorted.emit()
         self.list_changed.emit()
 
     def clear(self):
+        """Empty the list in-place."""
         super().clear()
         self.list_cleared.emit()
         self.list_changed.emit()
@@ -138,41 +162,49 @@ class ObservableList(list):
         return f"ObservableList({super().__str__()})"
 
 
-class ObservableDict(dict):
+class ObservableDict(dict[K, T]):
     """Interface to a dict that makes operations observable."""
 
-    item_set: EventHook[Hashable, Any] = EventHook()
-    item_popped: EventHook[Hashable, Any] = EventHook()
+    item_set: EventHook[K, T] = EventHook()
+    item_popped: EventHook[tuple[K, T | None]] = EventHook()
     cleared = EventHook()
-    updated: EventHook[dict] = EventHook()
+    updated: EventHook[dict[K, T]] = EventHook()
     changed = EventHook()
 
-    def pop(self, key, default=None):
+    def pop(self, key: K, default=None) -> T | None:
+        """Remove and return the value for the given key.
+
+        If the key is not found, return the default value.
+        """
         value = super().pop(key, default)
-        self.item_popped.emit(key, value)
+        self.item_popped.emit((key, value))
         self.changed.emit()
         return value
 
-    def popitem(self):
-        key, value = super().popitem()
-        self.item_popped.emit(key, value)
+    def popitem(self) -> tuple[K, T]:
+        """Remove and return the last entry in the dict as a (key, value) pair."""
+        item = super().popitem()
+        self.item_popped.emit(item)
         self.changed.emit()
-        return key, value
+        return item
 
-    def setdefault(self, key, default=None):
+    def setdefault(self, key: K, default: T) -> T:
+        """Set key's value to `default` if not found. Return the stored value."""
         value = super().setdefault(key, default)
         self.item_set.emit(key, value)
         self.changed.emit()
         return value
 
     def clear(self):
+        """Remove all items from the dict in-place."""
         super().clear()
         self.cleared.emit()
         self.changed.emit()
 
-    def update(self, other):
+    def update(self, other: Mapping[K, T]):
+        """Update the dict in-place with the given mapping."""
         super().update(other)
-        self.updated.emit(other)
+        self.updated.emit(dict(other))
         self.changed.emit()
 
     def __hash__(self):
